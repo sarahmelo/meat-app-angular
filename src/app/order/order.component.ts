044efd69e72125b5a815e085db65b95eef0172e0
sarahmelo <67019242+sarahmelo@users.noreply.github.com>
2021-07-27 16:48:40 -0300

    Reactive Forms: Validations [With patterns]

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CartItemModel } from 'app/restaurant-detail/shopping-cart/cart-Item.model';
 import { RadioOption } from 'app/shared/radio/radio-option.model';
@@ -15,6 +15,8 @@ export class OrderComponent implements OnInit {
   delivery: number = 8;
 
   orderForm: FormGroup
+  emailPattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+  numberPattern = /^[0-9]*$/
 
 
   paymentOption: RadioOption[] = [
@@ -31,13 +33,13 @@ export class OrderComponent implements OnInit {
   ngOnInit() {
 
     this.orderForm  = this.formBuilder.group({
-      name:  this.formBuilder.control(''),
-      email: this.formBuilder.control(''),
-      emailConfirmation:  this.formBuilder.control(''),
-      address:  this.formBuilder.control(''),
-      number:  this.formBuilder.control(''),
-      optionalAddress:  this.formBuilder.control(''),
-      paymentOption:  this.formBuilder.control('')
+      name: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+      email: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      emailConfirmation: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      address: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+      number: this.formBuilder.control('', [Validators.required, Validators.pattern(this.numberPattern)]),
+      optionalAddress: this.formBuilder.control(''),
+      paymentOption: this.formBuilder.control('', [Validators.required])
     })
   }
 

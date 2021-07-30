dea40e96f49e9aaee5b009cd5f7df398224f4805
sarahmelo <67019242+sarahmelo@users.noreply.github.com>
2021-07-30 17:28:30 -0300

    Lazy-loading: AboutModule

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from "@angular/router";
-import { AboutComponent } from "./about/about.component";
 import { HomeComponent } from "./home/home.component";
 import { OrderSummaryComponent } from "./order-summary/order-summary.component";
 import { OrderComponent } from "./order/order.component";
@@ -10,7 +9,7 @@ import { RestaurantsComponent } from "./restaurants/restaurants.component";
 
 export const ROUTES: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'about', component: AboutComponent},
+    {path: 'about', loadChildren:'./about/about.module#AboutModule'},
     {path: 'restaurants', component: RestaurantsComponent},
     {path: 'restaurants/:id', component: RestaurantDetailComponent, 
         children: [

d5a4b17d77c0aab184c77aed1604053cda525408
sarahmelo <67019242+sarahmelo@users.noreply.github.com>
2021-07-30 18:25:32 -0300

    Lazy-loading: OrderModule

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -9,7 +9,7 @@ const ROUTES: Routes = [
 
 @NgModule({
     declarations: [AboutComponent],
-    imports: [RouterModule, RouterModule.forChild(ROUTES)]
+    imports: [RouterModule.forChild(ROUTES)]
 })
 
 export class AboutModule{}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,12 +21,10 @@ import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shoppin
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping.cart.service';
-import { OrderComponent } from './order/order.component';
-import { OrderItemsComponent } from './order/order-items/order-items.component';
 import { OrderService } from './order/order.service';
-import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { SharedModule } from './shared/shared.module';
+import { OrderModule } from './order/order.module';
 @NgModule({
   declarations: [
     AppComponent,
@@ -39,9 +37,6 @@ import { SharedModule } from './shared/shared.module';
     ShoppingCartComponent,
     MenuItemComponent,
     ReviewsComponent,
-    OrderComponent,
-    OrderItemsComponent,
-    DeliveryCostsComponent,
     OrderSummaryComponent,
   ],
   imports: [

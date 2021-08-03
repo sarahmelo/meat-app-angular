24c34fd6547e017483176204eec0f0eb4fa2d04c
sarahmelo <67019242+sarahmelo@users.noreply.github.com>
2021-08-03 17:25:36 -0300

    animations: MenuItem

--- src/app/restaurant-detail/menu-item/menu-item.component.ts
+++ src/app/restaurant-detail/menu-item/menu-item.component.ts
@@ -2,15 +2,27 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { RestaurantsService } from 'app/restaurants/restaurants.service';
 import { Observable } from 'rxjs/Observable';
 import { MenuItemModel } from './menu-item.model';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'mt-menu-item',
   templateUrl: './menu-item.component.html',
+  animations: [
+    trigger('menuItemAppread',[
+      state('ready', style({opacity: 1})),
+      transition('void => ready', [
+        style({opacity: 0, transform: 'translateY(-10px)'}),
+        animate('500ms 0s ease-in')
+      ])
+    ])
+  ]
 })
 export class MenuItemComponent implements OnInit {
 
   @Input() menuItem: MenuItemModel
   @Output() add = new EventEmitter()
+
+  menuItemState = 'ready'
 
   constructor() { }
 
